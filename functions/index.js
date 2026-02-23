1b883664c222e834db99fdb2c232ec12038509c5
Mario Smith <mario@brighthornmedia.com>
2026-02-23 14:20:57 -0500

    fix nexserver to no longer be shared

--- functions/index.js
+++ functions/index.js
@@ -4,19 +4,19 @@ const { onRequest } = require("firebase-functions/v2/https");
 const { defineSecret } = require("firebase-functions/params");
 const { Storage } = require("@google-cloud/storage");
 
-// GCS_PRIVATE_KEY stored in Firebase Secret Manager
+// CITIGROVE_GCS_PRIVATE_KEY stored in Firebase Secret Manager
 // GCS_CLIENT_EMAIL and GCS_BUCKET_NAME are set via functions/.env
-const gcsPrivateKey = defineSecret("GCS_PRIVATE_KEY");
+const citigroveGcsPrivateKey = defineSecret("CITIGROVE_GCS_PRIVATE_KEY");
 
 const EXPIRY_MS = 15 * 60 * 1000;
 
-exports.nextServer = onRequest(
+exports.citigroveMedia = onRequest(
   {
     region: "us-east1",
     memory: "256MiB",
     timeoutSeconds: 30,
     concurrency: 80,
-    secrets: [gcsPrivateKey],
+    secrets: [citigroveGcsPrivateKey],
   },
   async (req, res) => {
     res.set("Access-Control-Allow-Origin", "*");
@@ -44,7 +44,7 @@ exports.nextServer = onRequest(
         projectId: "organic-spirit-488116-e2",
         credentials: {
           client_email: process.env.GCS_CLIENT_EMAIL,
-          private_key: (process.env.GCS_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
+          private_key: (citigroveGcsPrivateKey.value() || "").replace(/\\n/g, "\n"),
         },
       });
 
